8a363d3f60a0561a242d75b79ecc1c9d25627cc6
Rafael Zerunian <41594403+rzerunian@users.noreply.github.com>
2022-12-21 18:02:25 -0300

    Update script.js

--- dice/script.js
+++ dice/script.js
@@ -22,17 +22,3 @@ iniciativa.addEventListener('click', async () => {
        });
     console.log(await response.json());
 });
-
-/*
-const ataqueDistancia = document.getElementById('ataque-distancia');
-ataqueDistancia.addEventListener('click', () => {
-  fetch('http://191.9.126.30:2456/run-node-ataque-distancia')
-    .then(response => response.text())
-});
-
-const ataqueCorpo = document.getElementById('ataque-corpo');
-ataqueCorpo.addEventListener('click', () => {
-  fetch('http://191.9.126.30:2456/run-node-ataque-corpo')
-    .then(response => response.text())
-});
-*/
