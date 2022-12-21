94a0e483ed92718ce7750dce6e3f10c02316cab1
Rafael Zerunian <41594403+rzerunian@users.noreply.github.com>
2022-12-21 18:12:00 -0300

    Update script.js

--- dice/script.js
+++ dice/script.js
@@ -1,7 +1,7 @@
 const iniciativa = document.getElementById('iniciativa');
 iniciativa.addEventListener('click', async () => {
-    const pageId = 'cc50ac5544ea412fb2cb37daca877934'; // Replace with the ID of the page you want to update
-    const databaseId = '0be1ac8fb627477280713eb49f0a7cce'; // Replace with the ID of the database you want to update
+    const pageId = '0be1ac8f-b627-4772-8071-3eb49f0a7cce'; // Replace with the ID of the page you want to update
+    const databaseId = '67dcac7a-ef27-409e-9857-fc40208b8953'; // Replace with the ID of the database you want to update
     const property = 'Iniciativa'; // Replace with the name of the property you want to update
     const value = '5'; // Replace with the new value you want to set
 
