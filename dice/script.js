ed9f7cdedba1906e54b0cabbc8216c790a75ea67
Rafael Zerunian <41594403+rzerunian@users.noreply.github.com>
2022-12-21 18:00:40 -0300

    Update script.js

--- dice/script.js
+++ dice/script.js
@@ -1,9 +1,29 @@
 const iniciativa = document.getElementById('iniciativa');
-iniciativa.addEventListener('click', () => {
-  fetch('http://191.9.126.30:2456/run-node-iniciativa')
-    .then(response => response.text())
+iniciativa.addEventListener('click', async () => {
+    const pageId = 'cc50ac5544ea412fb2cb37daca877934'; // Replace with the ID of the page you want to update
+    const databaseId = '0be1ac8fb627477280713eb49f0a7cce'; // Replace with the ID of the database you want to update
+    const property = 'Iniciativa'; // Replace with the name of the property you want to update
+    const value = '5'; // Replace with the new value you want to set
+
+    const response = await fetch(`https://api.notion.com/v3/updatePage`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer secret_Hz5rN4bBP6kXPsOaZERP35OMagBipwN5sumECgVia8W`
+      },
+      body: JSON.stringify({
+        pageId: pageId,
+        properties: {
+          [property]: {
+            rich_text: [[value]]
+          }
+         }
+        })
+       });
+    console.log(await response.json());
 });
 
+/*
 const ataqueDistancia = document.getElementById('ataque-distancia');
 ataqueDistancia.addEventListener('click', () => {
   fetch('http://191.9.126.30:2456/run-node-ataque-distancia')
@@ -15,3 +35,4 @@ ataqueCorpo.addEventListener('click', () => {
   fetch('http://191.9.126.30:2456/run-node-ataque-corpo')
     .then(response => response.text())
 });
+*/
